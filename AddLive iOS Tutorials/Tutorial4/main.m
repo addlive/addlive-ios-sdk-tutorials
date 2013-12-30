b6d2e468a9e18aea6183e27b2acb1ca868add31d
Tadeusz Kozak <ted@addlive.com>
2013-12-30 22:50:44 +0100

    Added tutorial 4 - speech activity handling

---
+++ AddLive iOS Tutorials/Tutorial4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tutorial4
+//
+//  Created by Tadeusz Kozak on 30/12/13.
+//  Copyright (c) 2013 AddLive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ALAppDelegate class]));
+    }
+}
